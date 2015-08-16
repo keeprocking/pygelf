9184f7eaadee88e7bedb0a76b710ee9507da0818
Ivan Mukhin <muhin.ivan@gmail.com>
2015-08-16 12:52:50 +0300

    Change license in setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     author_email='muhin.ivan@gmail.com',
     url='https://github.com/keeprocking/pygelf',
     long_description=open('README.md').read(),
-    license='LICENSE.txt'
+    license='MIT'
 )
