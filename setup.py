ef3f51f7b0f5726c936e7391f1d080d57a7704d7
Ivan Mukhin <muhin.ivan@gmail.com>
2015-08-16 12:39:23 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name='pygelf',
+    version='0.1',
+    packages=['pygelf'],
+    description="Python logging implementation of GELF (graylog extended log format)",
+    keywords='logging udp tcp graylog2 graylog gelf',
+    author='Ivan Mukhin',
+    author_email='muhin.ivan@gmail.com',
+    url='https://github.com/keeprocking/pygelf',
+    long_description=open('README.md').read(),
+    license='MIT License'
+)
