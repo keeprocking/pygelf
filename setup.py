6266415757ac24681f6bfa490525a5496530e5dc
Ivan Mukhin <muhin.ivan@gmail.com>
2015-08-16 13:21:32 +0300

    Replace md with rst

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     author='Ivan Mukhin',
     author_email='muhin.ivan@gmail.com',
     url='https://github.com/keeprocking/pygelf',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     license='MIT'
 )
