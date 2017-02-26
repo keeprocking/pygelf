060b7fe6631aabf59615de004a0f0f274106c233
Ivan Mukhin <muhin.ivan@gmail.com>
2017-02-26 15:40:31 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pygelf',
-    version='0.2.9',
+    version='0.2.10',
     packages=['pygelf'],
     description='Logging handlers with GELF support',
     keywords='logging udp tcp ssl tls graylog2 graylog gelf',
