dba1f4b19c85977f80ef8b1f66d0d0cb0811eda8
Tom Cobb <tom.cobb@diamond.ac.uk>
2017-11-21 09:13:38 +0000

    Add support for using with a QueueHandler/QueueListener
    If Python3's QueueHandler/QueueListener are used then record.exc_info is
    None, but record.exc_text contains the traceback. Reading the source of
    the logging module it would appear that exc_text can safely be assumed
    to contain the exception text if it is not None, so use this to populate
    graylog's full_message

--- tests/common.py
+++ tests/common.py
@@ -4,15 +4,33 @@ import logging
 import json
 
 
+class FakeQueueHandler(logging.Handler):
+    def __init__(self, handler):
+        self.handler = handler
+        logging.Handler.__init__(self)
+
+    def emit(self, record):
+        # This is what python3's QueueHandler does to a logging record
+        # before it puts it on a Queue for a QueueListener to pick up
+        self.format(record)
+        record.msg = record.message
+        record.args = None
+        record.exc_info = None
+        self.handler.handle(record)
+
+
 @pytest.yield_fixture
 def send(handler):
     with mock.patch.object(handler, 'send') as mock_send:
         yield mock_send
 
 
-@pytest.yield_fixture
-def logger(handler):
+@pytest.yield_fixture(params=[True, False])
+def logger(request, handler):
     logger = logging.getLogger('test')
+    if request.param:
+        # Should be queued
+        handler = FakeQueueHandler(handler)
     logger.addHandler(handler)
     yield logger
     logger.removeHandler(handler)
