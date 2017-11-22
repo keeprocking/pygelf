8f7f006cf4925ff5bc1b5cce513c1e75b1a545fa
Ivan Mukhin <muhin.ivan@gmail.com>
2017-11-22 22:29:58 +0300

    increase sleep duration

--- tests/helper.py
+++ tests/helper.py
@@ -44,7 +44,7 @@ def _build_api_string(message, fields):
 
 
 def _get_api_response(message, fields):
-    time.sleep(2)
+    time.sleep(3)
     url = _build_api_string(message, fields)
     api_response = requests.get(url, auth=('admin', 'admin'), headers={'accept': 'application/json'})
     return api_response
