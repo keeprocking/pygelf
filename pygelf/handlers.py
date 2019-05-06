3d307ac60d6b404e872ab60eaf0c181a9014a9d4
samuel <samuel@meyn.fr>
2019-05-06 15:41:43 +0200

    add OVH Handler for OVH Logs Data Platform

--- pygelf/handlers.py
+++ pygelf/handlers.py
@@ -159,3 +159,22 @@ class GelfHttpHandler(BaseHandler, LoggingHandler):
         data = self.convert_record_to_gelf(record)
         connection = httplib.HTTPConnection(host=self.host, port=self.port, timeout=self.timeout)
         connection.request('POST', self.path, data, self.headers)
+
+
+class GelfOVHHandler(GelfTcpHandler):
+    """
+    Class to adapt OVH Log management platform
+    """
+    def __init__(self, ovh_token, **kwargs):
+        """
+        OVH TCP Handler
+        :param ovh_token: OVH API Token
+        """
+        self.ovh_token = ovh_token
+        GelfTcpHandler.__init__(self, **kwargs)
+
+    def convert_record_to_gelf(self, record):
+        l = gelf.make(record, self.domain, self.debug, self.version,
+                      self.additional_fields, self.include_extra_fields)
+        l.update({"_X-OVH-TOKEN": self.ovh_token})
+        return gelf.pack(l,self.compress, self.json_default)
